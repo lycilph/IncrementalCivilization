249463c769ddec9ce4b6545b0903581d44ec05a2
Morten Lang <Morten Lang@DK-LPT-ML>
2024-03-22 12:43:20 +0100

    Refactored views

---
+++ IncrementalCivilization/Mvvm/Controls/BuildingsViewer.xaml.cs
@@ -0,0 +1,30 @@
+﻿using IncrementalCivilization.Domain;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IncrementalCivilization.Mvvm.Controls;
+
+public partial class BuildingsViewer : UserControl
+{
+    public ObservableCollection<Building> BuildingsSource
+    {
+        get { return (ObservableCollection<Building>)GetValue(BuildingsSourceProperty); }
+        set { SetValue(BuildingsSourceProperty, value); }
+    }
+    public static readonly DependencyProperty BuildingsSourceProperty =
+        DependencyProperty.Register("BuildingsSource", typeof(ObservableCollection<Building>), typeof(BuildingsViewer), new PropertyMetadata(null));
+
+    public ResourceBundle ResourcesSource
+    {
+        get { return (ResourceBundle)GetValue(ResourcesSourceProperty); }
+        set { SetValue(ResourcesSourceProperty, value); }
+    }
+    public static readonly DependencyProperty ResourcesSourceProperty =
+        DependencyProperty.Register("ResourcesSource", typeof(ResourceBundle), typeof(BuildingsViewer), new PropertyMetadata(null));
+
+    public BuildingsViewer()
+    {
+        InitializeComponent();
+    }
+}
---
+++ IncrementalCivilization/Mvvm/Controls/ResourcesViewer.xaml.cs
@@ -0,0 +1,22 @@
+﻿using IncrementalCivilization.Domain;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IncrementalCivilization.Mvvm.Controls;
+
+public partial class ResourcesViewer : UserControl
+{
+    public ObservableCollection<Resource> ItemsSource
+    {
+        get { return (ObservableCollection<Resource>)GetValue(ItemsSourceProperty); }
+        set { SetValue(ItemsSourceProperty, value); }
+    }
+    public static readonly DependencyProperty ItemsSourceProperty =
+        DependencyProperty.Register("ItemsSource", typeof(ObservableCollection<Resource>), typeof(ResourcesViewer), new PropertyMetadata(null));
+
+    public ResourcesViewer()
+    {
+        InitializeComponent();
+    }
+}
