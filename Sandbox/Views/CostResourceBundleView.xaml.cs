946ce08c5006e8d0c9df70e9c2cb05cf1341ce0c
Morten Lang <Morten Lang@DK-LPT-ML>
2024-03-26 11:03:59 +0100

    Cost added to building

---
+++ Sandbox/Views/CostResourceBundleView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Sandbox.Views;
+
+public partial class CostResourceBundleView : UserControl
+{
+    public CostResourceBundleView()
+    {
+        InitializeComponent();
+    }
+}
